f710d377d43d470cccdd164daabc4c795a4c324f
mushfiq <mushfiqxrabbi@gmail.com>
2023-10-20 04:41:07 +0600

    ...

--- lua/custom/plugins/action-hints.lua
+++ lua/custom/plugins/action-hints.lua
@@ -1,17 +1,17 @@
 return {
-  'roobert/action-hints.nvim',
-  config = function()
-    require('action-hints').setup {
-      template = {
-        definition = { text = ' ⊛', color = '#add8e6' },
-        references = { text = ' ↱%s', color = '#ff6666' },
-      },
-      use_virtual_text = true,
-    }
-    require('lualine').setup {
-      sections = {
-        lualine_x = { require('action-hints').statusline },
-      },
-    }
-  end,
+  -- 'roobert/action-hints.nvim',
+  -- config = function()
+  --   require('action-hints').setup {
+  --     template = {
+  --       definition = { text = ' ⊛', color = '#add8e6' },
+  --       references = { text = ' ↱%s', color = '#ff6666' },
+  --     },
+  --     use_virtual_text = true,
+  --   }
+  --   require('lualine').setup {
+  --     sections = {
+  --       lualine_x = { require('action-hints').statusline },
+  --     },
+  --   }
+  -- end,
 }
