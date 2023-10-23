793828755ce8fc1efd00e40ca4f145495cfe2a34
mushfiq <mushfiqxrabbi@gmail.com>
2023-10-23 13:32:19 +0600

    ...

--- lua/custom/plugins/alpha-nvim.lua
+++ lua/custom/plugins/alpha-nvim.lua
@@ -11,13 +11,17 @@ return {
 
     dashboard.section.buttons.val = {
       -- dashboard.button('e', '  New file', ':ene <BAR> startinsert <CR>'),
-      dashboard.button('SPC f p', '  Projects', ':Telescope projects<CR>'),
-      dashboard.button('SPC F F', '󰱽  Find file', ':lua require("telescope.builtin").find_files({ cwd = vim.env.HOME, follow = true, hidden = true })<CR>'),
-      dashboard.button('SPC f o', '  Recent files', ':Telescope oldfiles<CR>'),
-      dashboard.button('SPC o o', '  File brwoser', ':Oil<cr>'),
+      dashboard.button('SPC s p', '  Projects', ':Telescope projects<CR>'),
+      dashboard.button(
+        'SPC s F',
+        '󰱽  Search file',
+        ':lua require("telescope.builtin").find_files({ cwd = vim.env.HOME, follow = true, hidden = true })<CR>'
+      ),
+      dashboard.button('SPC s o', '  Recent files', ':Telescope oldfiles<CR>'),
+      dashboard.button('SPC o', '  File brwoser', ':Oil<cr>'),
       -- dashboard.button('SPC l g', '󰺯  Live grep', ':Telescope live_grep<CR>'),
-      -- dashboard.button('SPC f w', '󰈭  Find word', ':Telescope grep_string<CR>'),
-      dashboard.button('SPC f h', '󰋖  Help', ':Telescope help_tags<CR>'),
+      -- dashboard.button('SPC s w', '󰈭  Find word', ':Telescope grep_string<CR>'),
+      dashboard.button('SPC s h', '󰋖  Help', ':Telescope help_tags<CR>'),
       dashboard.button('q', '󰅚  Quit NVIM', ':qa<CR>'),
     }
 
