e7398986f5d574c35793858bfe20444c62de056e
mushfiq <mushfiqxrabbi@gmail.com>
2023-10-23 13:18:30 +0600

    ...

--- init.lua
+++ init.lua
@@ -531,15 +531,6 @@ vim.defer_fn(function()
         },
       },
     },
-    rainbow = {
-      enable = true,
-      -- list of languages you want to disable the plugin for
-      disable = {},
-      -- Which query to use for finding delimiters
-      query = 'rainbow-parens',
-      -- Highlight the entire buffer all at once
-      strategy = require('ts-rainbow').strategy.global,
-    },
   }
 end, 0)
 
