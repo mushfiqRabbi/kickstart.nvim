73354636910d7d089b80844e19be04f83c64ee64
mushfiq <mushfiqxrabbi@gmail.com>
2023-10-25 11:50:42 +0600

    ...

--- lua/custom/plugins/oil.lua
+++ lua/custom/plugins/oil.lua
@@ -1,7 +1,7 @@
 return {
   'stevearc/oil.nvim',
   opts = {
-    -- cleanup_delay_ms = 0,
+    -- cleanup_delay_ms = false,
     -- win_options = {
     --   wrap = true,
     -- },
@@ -9,27 +9,39 @@ return {
   -- Optional dependencies
   dependencies = { 'nvim-tree/nvim-web-devicons' },
   config = function()
+    -- local oil = require 'oil'
+    -- local util = require 'oil.util'
     require('oil').setup {
       skip_confirm_for_simple_edits = true,
       keymaps = {
         ['<C-s>'] = false,
+        ['<CR>'] = {
+          callback = function()
+            local oil = require 'oil'
+            oil.select()
+            if oil.get_cursor_entry().type == 'directory' then
+              oil.select { preview = true }
+            end
+          end,
+          mode = 'n',
+        },
         ['<C-c>'] = {
           callback = function()
             local oil = require 'oil'
             local util = require 'oil.util'
-            local entry = oil.get_cursor_entry()
-            if not entry then
-              vim.notify('Could not find entry under cursor', vim.log.levels.ERROR)
-              return
-            end
-            local winid = util.get_preview_win()
-            if winid then
-              local cur_id = vim.w[winid].oil_entry_id
-              if entry.id == cur_id then
-                vim.api.nvim_win_close(winid, true)
-                oil.close()
-                return
-              end
+            -- local entry = oil.get_cursor_entry()
+            -- if not entry then
+            --   vim.notify('Could not find entry under cursor', vim.log.levels.ERROR)
+            --   return
+            -- end
+            local preview_win = util.get_preview_win()
+            if preview_win then
+              -- local cur_id = vim.w[winid].oil_entry_id
+              -- if entry.id == cur_id then
+              vim.api.nvim_win_close(preview_win, true)
+              -- oil.close()
+              -- return
+              -- end
             end
             oil.close()
           end,
