4f97ad76d919b8447de0b21294a61cfe53fee6b4
Mushfiqur Rahman <mushfiqxrabbi@gmail.com>
2023-10-27 17:06:14 +0600

    ...

--- lua/custom/plugins/project.lua
+++ lua/custom/plugins/project.lua
@@ -3,7 +3,8 @@ return {
   config = function()
     require('project_nvim').setup {
       -- manual_mode = true,
-      detection_methods = { 'pattern' },
+      -- detection_methods = { 'pattern' },
+      detection_methods = { 'pattern', 'lsp' },
       show_hidden = true,
     }
   end,
