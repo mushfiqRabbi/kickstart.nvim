d35552028706f6c435df23f5c7c556981c47731c
Mushfiqur Rahman <mushfiqxrabbi@gmail.com>
2023-10-27 12:00:19 +0600

    ...

--- lua/custom/plugins/bufdel.lua
+++ lua/custom/plugins/bufdel.lua
@@ -4,7 +4,7 @@ return {
     require('bufdel').setup {
       quit = false,
     }
-    vim.keymap.set({ 'n', 'v', 'i' }, '<C-q>q', ':BufDel<cr>', { noremap = true, silent = true })
+    vim.keymap.set({ 'n', 'v', 'i' }, '<C-q><C-q>', ':BufDel<cr>', { noremap = true, silent = true })
     vim.keymap.set({ 'n', 'v', 'i' }, '<C-q>o', ':BufDelOthers<cr>', { noremap = true, silent = true })
     vim.keymap.set({ 'n', 'v', 'i' }, '<C-q>a', ':BufDelAll<cr>', { noremap = true, silent = true })
   end,
