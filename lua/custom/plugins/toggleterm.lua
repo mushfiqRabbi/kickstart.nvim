518b52748b8103bc6afcb5261907b1327c9819c7
Mushfiqur Rahman <mushfiqxrabbi@gmail.com>
2023-10-27 11:57:30 +0600

    ...

--- lua/custom/plugins/toggleterm.lua
+++ lua/custom/plugins/toggleterm.lua
@@ -2,6 +2,8 @@ return {
   'akinsho/toggleterm.nvim',
   version = '*',
   config = function()
+    local be
+    local bl
     function _G.set_terminal_keymaps()
       local opts = { buffer = 0 }
       vim.keymap.set('t', '<esc>', [[<C-\><C-n>]], opts)
@@ -11,7 +13,8 @@ return {
       -- vim.keymap.set('t', '<C-k>', [[<Cmd>wincmd k<CR>]], opts)
       -- vim.keymap.set('t', '<C-l>', [[<Cmd>wincmd l<CR>]], opts)
       vim.keymap.set('t', '<C-w>', [[<C-\><C-n><C-w>]], opts)
-      vim.keymap.set('t', '<C-q>', 'exit<cr>', opts)
+      vim.keymap.set('t', '<C-w>q', 'exit<cr>', opts)
+      vim.keymap.set('n', '<C-w>q', 'iexit<cr>', opts)
     end
 
     -- if you only want these mappings for toggle term use term://*toggleterm#* instead
@@ -20,10 +23,10 @@ return {
     require('toggleterm').setup {
       size = function(term)
         if term.direction == 'horizontal' then
-          if math.floor(vim.o.lines * 0.3) < 16 then
-            return 16
-          else
-            return math.floor(vim.o.lines * 0.3)
+          if vim.fn.hostname() == 'DESKTOP-QP9KQE3' then
+            return vim.o.lines * 0.3
+          elseif vim.fn.hostname() == 'DESKTOP-CKMOCDG' then
+            return vim.o.lines * 0.4
           end
         elseif term.direction == 'vertical' then
           return vim.o.columns * 0.4
@@ -32,6 +35,50 @@ return {
       persist_mode = false,
       persist_size = false,
       open_mapping = [[<c-\>]],
+      on_create = function()
+        -- print('on_create', term.bufnr)
+      end,
+      on_open = function()
+        -- print('on_open', term.bufnr)
+        vim.cmd 'wincmd w'
+        vim.cmd 'stopinsert'
+        be = vim.api.nvim_create_autocmd('BufEnter', {
+          pattern = 'term://*#toggleterm#*',
+          callback = function()
+            vim.cmd 'resize'
+            -- vim.cmd 'startinsert'
+          end,
+        })
+        bl = vim.api.nvim_create_autocmd('BufLeave', {
+          pattern = 'term://*#toggleterm#*',
+          callback = function()
+            vim.cmd(string.format('resize %s', vim.o.lines * 0.4))
+            -- vim.cmd 'startinsert'
+          end,
+        })
+      end,
+      on_close = function()
+        -- print('on_close', term.bufnr)
+        vim.api.nvim_del_autocmd(be)
+        vim.api.nvim_del_autocmd(bl)
+      end,
+      on_exit = function()
+        -- print('on_exit', term)
+      end,
     }
+    -- vim.api.nvim_create_autocmd('BufEnter', {
+    --   pattern = 'term://*#toggleterm#*',
+    --   callback = function(ev)
+    --     vim.cmd 'resize'
+    --     -- vim.cmd 'startinsert'
+    --   end,
+    -- })
+    -- vim.api.nvim_create_autocmd('BufLeave', {
+    --   pattern = 'term://*#toggleterm#*',
+    --   callback = function(ev)
+    --     vim.cmd(string.format('resize %s', vim.o.lines * 0.4))
+    --     -- vim.cmd 'startinsert'
+    --   end,
+    -- })
   end,
 }
