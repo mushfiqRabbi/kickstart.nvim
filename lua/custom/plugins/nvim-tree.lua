7fe04f3902174d74c981489721b2125ab091d4f9
Mushfiqur Rahman <mushfiqxrabbi@gmail.com>
2023-11-07 21:14:20 +0600

    ...

--- lua/custom/plugins/nvim-tree.lua
+++ lua/custom/plugins/nvim-tree.lua
@@ -13,6 +13,13 @@ return {
     local HEIGHT_RATIO = 0.8 -- You can change this
     local WIDTH_RATIO = 0.5 -- You can change this too
     require('nvim-tree').setup {
+      on_attach = function(bufnr)
+        local api = require 'nvim-tree.api'
+        local function opts(desc)
+          return { desc = 'nvim-tree: ' .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
+        end
+      end,
+
       diagnostics = {
         enable = true,
         show_on_dirs = true,
