85791a3af4e53789e716c3fbaf1f49965b850473
Mushfiqur Rahman <mushfiqxrabbi@gmail.com>
2023-11-08 20:19:09 +0600

    ...

--- lua/custom/plugins/modicator.lua
+++ lua/custom/plugins/modicator.lua
@@ -1,7 +1,10 @@
 return {
   'mawkler/modicator.nvim',
   -- dependencies = 'navarasu/onedark.nvim', -- Add your colorscheme plugin here
-  dependencies = 'folke/tokyonight.nvim', -- Add your colorscheme plugin here
+  dependencies = {
+    'catppuccin/nvim',
+    name = 'catppuccin',
+  }, -- Add your colorscheme plugin here
   event = 'VeryLazy',
   init = function()
     -- These are required for Modicator to work
