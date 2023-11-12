89ca456443c86947a9a5e39c43d99ad2075efaaf
Mushfiqur Rahman <mushfiqxrabbi@gmail.com>
2023-11-12 17:04:21 +0600

    ...

--- lua/custom/plugins/smoothcursor.lua
+++ lua/custom/plugins/smoothcursor.lua
@@ -8,5 +8,27 @@ return {
         enable = true,
       },
     }
+    local autocmd = vim.api.nvim_create_autocmd
+
+    autocmd({ 'ModeChanged' }, {
+      callback = function()
+        local current_mode = vim.fn.mode()
+        if current_mode == 'n' then
+          vim.api.nvim_set_hl(0, 'SmoothCursor', { fg = '#8aadf4' })
+        elseif current_mode == 'v' then
+          vim.api.nvim_set_hl(0, 'SmoothCursor', { fg = '#c6a0f6' })
+        elseif current_mode == 'V' then
+          vim.api.nvim_set_hl(0, 'SmoothCursor', { fg = '#c6a0f6' })
+        elseif current_mode == '\22' then
+          vim.api.nvim_set_hl(0, 'SmoothCursor', { fg = '#c6a0f6' })
+        elseif current_mode == 'i' then
+          vim.api.nvim_set_hl(0, 'SmoothCursor', { fg = '#a6da95' })
+        elseif current_mode == 'R' then
+          vim.api.nvim_set_hl(0, 'SmoothCursor', { fg = '#ed8796' })
+        elseif current_mode == 'c' then
+          vim.api.nvim_set_hl(0, 'SmoothCursor', { fg = '#f5a97f' })
+        end
+      end,
+    })
   end,
 }
