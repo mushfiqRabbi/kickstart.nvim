85dea237cd8af439df23a8c8d03d123dfa04046f
Mushfiqur Rahman <mushfiqxrabbi@gmail.com>
2023-11-22 07:06:24 +0600

    ...

--- lua/custom/plugins/nvim-lightbulb.lua
+++ lua/custom/plugins/nvim-lightbulb.lua
@@ -10,6 +10,17 @@ return {
       },
     }
 
-    vim.cmd 'hi LightBulbNumber guifg=#fde154'
+    vim.api.nvim_create_autocmd('CursorHold', {
+      callback = function()
+        if vim.lsp.buf.server_ready() then
+          local r, _ = unpack(vim.api.nvim_win_get_cursor(0))
+          if next(vim.diagnostic.get(0, { lnum = r - 1 })) ~= nil then
+            vim.cmd 'hi LightBulbNumber gui=bold,italic guifg=#fde154'
+          else
+            vim.cmd 'hi LightBulbNumber gui=NONE guifg=#89b4fa'
+          end
+        end
+      end,
+    })
   end,
 }
