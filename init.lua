45e1defd4568bd71d5b867b96c053c11f47db82a
Mushfiqur Rahman <mushfiqxrabbi@gmail.com>
2023-11-23 05:48:09 +0600

    ...

--- init.lua
+++ init.lua
@@ -312,7 +312,8 @@ require("lazy").setup({
           -- component_separators = { left = '', right = '' },
           component_separators = { left = "|", right = "|" },
           -- section_separators = { left = '', right = '' },
-          section_separators = { left = "|", right = "|" },
+          -- section_separators = { left = "|", right = "|" },
+          section_separators = "",
 
           -- theme = 'onedark',
           -- theme = 'tokyonight',
