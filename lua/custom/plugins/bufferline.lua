915e41ad137f727c194c5639af4df9ef20037828
Mushfiqur Rahman <mushfiqxrabbi@gmail.com>
2023-11-23 09:40:35 +0600

    ...

--- lua/custom/plugins/bufferline.lua
+++ lua/custom/plugins/bufferline.lua
@@ -1,21 +1,21 @@
 return {
-  'akinsho/bufferline.nvim',
+  "akinsho/bufferline.nvim",
   -- after = 'catppuccin',
-  version = '*',
+  version = "*",
   dependencies = {
     {
-      'catppuccin/nvim',
-      name = 'catppuccin',
+      "catppuccin/nvim",
+      name = "catppuccin",
     },
-    'nvim-tree/nvim-web-devicons',
+    "nvim-tree/nvim-web-devicons",
   },
   config = function()
-    require('bufferline').setup {
+    require("bufferline").setup({
       -- highlights = require('catppuccin.groups.integrations.bufferline').get {},
       highlights = {
         indicator_selected = {
-          fg = '#f5a97f',
-          bg = '#24273a',
+          fg = "#f5a97f",
+          bg = "#24273a",
         },
       },
       options = {
@@ -23,10 +23,13 @@ return {
         -- indicator = {
         --   style = 'underline',
         -- },
-        diagnostics = 'nvim_lsp',
+        diagnostics = "nvim_lsp",
         diagnostics_indicator = function(count, level)
-          local icon = level:match 'error' and ' ' or level:match 'warn' and ' ' or level:match 'hint' and ' ' or ' '
-          return ' ' .. icon .. count
+          local icon = level:match("error") and " "
+            or level:match("warn") and " "
+            or level:match("hint") and " "
+            or " "
+          return " " .. icon .. count
         end,
         -- offsets = {
         --   {
@@ -48,27 +51,27 @@ return {
             local hint = #vim.diagnostic.get(0, { severity = seve.HINT })
 
             if error ~= 0 then
-              table.insert(result, { text = '  ' .. error, fg = '#ed8796' })
+              table.insert(result, { text = "  " .. error, fg = "#ed8796" })
             end
 
             if warning ~= 0 then
-              table.insert(result, { text = '  ' .. warning, fg = '#eed49f' })
+              table.insert(result, { text = "  " .. warning, fg = "#eed49f" })
             end
 
             if hint ~= 0 then
-              table.insert(result, { text = '  ' .. hint, fg = '#8bd5ca' })
+              table.insert(result, { text = "  " .. hint, fg = "#8bd5ca" })
             end
 
             if info ~= 0 then
-              table.insert(result, { text = '  ' .. info, fg = '#91d7e3' })
+              table.insert(result, { text = "  " .. info, fg = "#91d7e3" })
             end
             return result
           end,
         },
         -- separator_style = 'thick',
       },
-    }
-    vim.keymap.set({ 'n', 'i', 'v' }, '<C-l>', '<cmd>BufferLineCycleNext<cr>', { noremap = true, silent = true })
-    vim.keymap.set({ 'n', 'i', 'v' }, '<C-h>', '<cmd>BufferLineCyclePrev<cr>', { noremap = true, silent = true })
+    })
+    vim.keymap.set({ "n", "i", "v" }, "<C-l>", "<cmd>BufferLineCycleNext<cr>", { noremap = true, silent = true })
+    vim.keymap.set({ "n", "i", "v" }, "<C-h>", "<cmd>BufferLineCyclePrev<cr>", { noremap = true, silent = true })
   end,
 }
