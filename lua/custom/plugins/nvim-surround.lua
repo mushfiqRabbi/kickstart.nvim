37322e32388dc20af29dadad39a62f3d2cf8392d
Mushfiqur Rahman <mushfiqxrabbi@gmail.com>
2023-11-25 03:49:08 +0600

    ...

--- lua/custom/plugins/nvim-surround.lua
+++ lua/custom/plugins/nvim-surround.lua
@@ -1,14 +1,13 @@
 return {
-  'kylechui/nvim-surround',
-  version = '*', -- Use for stability; omit to use `main` branch for the latest features
-  event = 'VeryLazy',
+  "kylechui/nvim-surround",
+  event = "VeryLazy",
   config = function()
-    require('nvim-surround').setup {
-      -- Configuration here, or leave empty to use defaults
+    require("nvim-surround").setup({
       keymaps = {
         insert = false,
         insert_line = false,
       },
-    }
+      move_cursor = false,
+    })
   end,
 }
