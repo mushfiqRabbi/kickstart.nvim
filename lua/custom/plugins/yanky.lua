8b61cc903d57c29297cb3bbc4cad11e3aae36143
Mushfiqur Rahman <mushfiqxrabbi@gmail.com>
2023-11-25 02:36:20 +0600

    ...

--- lua/custom/plugins/yanky.lua
+++ lua/custom/plugins/yanky.lua
@@ -1,14 +1,14 @@
 return {
-  'gbprod/yanky.nvim',
+  "gbprod/yanky.nvim",
   dependencies = {
-    { 'mg979/vim-visual-multi', branch = 'master' },
+    { "mg979/vim-visual-multi", branch = "master" },
   },
   config = function()
-    require('yanky').setup {}
-    vim.keymap.set({ 'n', 'x' }, 'p', '<Plug>(YankyPutAfter)')
-    vim.keymap.set({ 'n', 'x' }, 'P', '<Plug>(YankyPutBefore)')
-    vim.keymap.set({ 'n', 'x' }, 'gp', '<Plug>(YankyGPutAfter)')
-    vim.keymap.set({ 'n', 'x' }, 'gP', '<Plug>(YankyGPutBefore)')
+    require("yanky").setup({})
+    vim.keymap.set({ "n", "x" }, "p", "<Plug>(YankyPutAfter)")
+    vim.keymap.set({ "n", "x" }, "P", "<Plug>(YankyPutBefore)")
+    vim.keymap.set({ "n", "x" }, "gp", "<Plug>(YankyGPutAfter)")
+    vim.keymap.set({ "n", "x" }, "gP", "<Plug>(YankyGPutBefore)")
     -- vim.keymap.set('n', '<c-n>', '<Plug>(YankyCycleForward)')
     -- vim.keymap.set('n', '<c-p>', '<Plug>(YankyCycleBackward)')
   end,
