5a6dd402fed87fdd4fc165fec9b2d8ffb998b5d4
Mushfiqur Rahman <mushfiqxrabbi@gmail.com>
2023-11-28 22:19:39 +0600

    ...

--- lua/custom/plugins/auto-session.lua
+++ lua/custom/plugins/auto-session.lua
@@ -1,10 +1,8 @@
 return {
-  -- 'rmagatti/auto-session',
-  -- config = function()
-  --   require('auto-session').setup {
-  --     auto_restore_enabled = true,
-  --     log_level = 'error',
-  --     auto_session_suppress_dirs = { '~/', '~/Projects', '~/Downloads', '/' },
-  --   }
-  -- end,
+  "rmagatti/auto-session",
+  config = function()
+    require("auto-session").setup({
+      auto_session_suppress_dirs = { "~/" },
+    })
+  end,
 }
