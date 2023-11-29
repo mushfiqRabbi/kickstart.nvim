d64b08aadd23252ddaa0d4d9e8c06f225037fa0e
Mushfiqur Rahman <mushfiqxrabbi@gmail.com>
2023-11-29 10:13:18 +0600

    ...

--- lua/custom/plugins/toggleterm.lua
+++ lua/custom/plugins/toggleterm.lua
@@ -121,8 +121,8 @@ return {
         },
       },
       float_opts = {
-        width = math.floor(vim.o.columns * 0.7),
-        height = math.floor(vim.o.lines * 0.8),
+        width = math.floor(vim.o.columns * 0.75),
+        height = math.floor(vim.o.lines * 0.75),
         winblend = 7,
       },
     })
