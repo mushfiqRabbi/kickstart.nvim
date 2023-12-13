268ecb69f1b771a9ef23244a2c2b7398b5e9527a
Mushfiqur Rahman <mushfiqxrabbi@gmail.com>
2023-12-14 05:35:33 +0600

    ...

--- lua/custom/plugins/noice.lua
+++ lua/custom/plugins/noice.lua
@@ -28,7 +28,13 @@ return {
           enabled = false,
         },
         signature = {
-          enabled = false,
+          opts = {
+            size = {
+              max_height = math.floor((vim.o.lines * 0.45) + 0.5),
+              max_width = math.floor((vim.o.columns * 0.6) + 0.5),
+            },
+            position = { row = 2, col = 0 },
+          },
         },
         override = {
           ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
