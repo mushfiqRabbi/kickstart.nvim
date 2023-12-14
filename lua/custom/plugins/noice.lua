cb210717820d19933114091ca1529be066471c6d
Mushfiqur Rahman <mushfiqxrabbi@gmail.com>
2023-12-14 12:47:21 +0600

    ...

--- lua/custom/plugins/noice.lua
+++ lua/custom/plugins/noice.lua
@@ -28,13 +28,17 @@ return {
           enabled = false,
         },
         signature = {
-          opts = {
-            size = {
-              max_height = math.floor((vim.o.lines * 0.45) + 0.5),
-              max_width = math.floor((vim.o.columns * 0.6) + 0.5),
-            },
-            position = { row = 2, col = 0 },
+          auto_open = {
+            enabled = false,
+            trigger = false,
           },
+          -- opts = {
+          --   size = {
+          --     max_height = math.floor((vim.o.lines * 0.45) + 0.5),
+          --     max_width = math.floor((vim.o.columns * 0.6) + 0.5),
+          --   },
+          --   position = { row = 2, col = 0 },
+          -- },
         },
         override = {
           ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
