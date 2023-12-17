9c443a144dcec53b9f97a39c153e38d64d53a986
mushfiq <mushfiqxrabbi@gmail.com>
2023-12-17 23:08:17 +0600

    ...

--- lua/custom/plugins/url-open.lua
+++ lua/custom/plugins/url-open.lua
@@ -1,13 +1,14 @@
 return {
-  -- 'git@github.com:mushfiqRabbi/url-open.git',
-  -- branch = 'tork',
-  -- event = 'VeryLazy',
-  -- cmd = 'URLOpenUnderCursor',
-  -- config = function()
-  --   local status_ok, url_open = pcall(require, 'url-open')
-  --   if not status_ok then
-  --     return
-  --   end
-  --   url_open.setup {}
-  -- end,
+  "sontungexpt/url-open",
+  event = "VeryLazy",
+  cmd = "URLOpenUnderCursor",
+  config = function()
+    local status_ok, url_open = pcall(require, "url-open")
+    if not status_ok then
+      return
+    end
+    url_open.setup({})
+
+    vim.keymap.set("n", "gx", "<esc>:URLOpenUnderCursor<cr>")
+  end,
 }
